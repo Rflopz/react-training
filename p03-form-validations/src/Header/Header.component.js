b27356c3694d762967cfe1549c9566a810a444fa
Roberto Garcia <roberto.garcia@xylem.com>
2022-11-03 14:14:45 -0600

    Complete header component

--- p03-form-validations/src/Header/Header.component.js
+++ p03-form-validations/src/Header/Header.component.js
@@ -1,13 +1,19 @@
-import React from "react";
-import logo from "../Shared/svg/logo.svg";
-import { Button } from "../Shared/components";
+import React, { useState } from "react";
+import { Button, Logo } from "../Shared/components";
 
 const Header = ({ title }) => {
+  const [rotateLogo, setRotateLogo] = useState(false);
+
   return (
     <header className="App-header">
-      <img src={logo} className="App-logo logo-animation" alt="logo" />
+      <Logo rotate={rotateLogo}></Logo>
       <h3>{title}</h3>
-      <Button title="Animation on" />
+      <Button
+        title={`Animation ${rotateLogo ? "on" : "off"}`}
+        onClick={() => {
+          setRotateLogo(!rotateLogo);
+        }}
+      />
     </header>
   );
 };
